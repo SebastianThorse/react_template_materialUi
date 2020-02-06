5740f0203b097a0d01eee7fac07ffdd0805a17ad
SebastianThorsen <sebbethorsen@gmail.com>
2020-02-06 16:51:32 +0100

    initial

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from '../pages/home/Home'
+import './App.scss';
+
+const App = () => {
+  return (
+    <>
+      <Home />
+    </>
+  );
+};
+
+export default App;
